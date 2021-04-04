cd0fb966476719c7912d530c6b239ed0f5b76769
Damilare <opeyemisamuel222@gmail.com>
2021-04-04 20:58:01 +0100

    async error handler

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20"),
   ],
-  (req: Request, res: Response) => {
+ async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,7 +23,7 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("creating a user");
+
     throw new DatabaseConnectionError();
     res.send({});
   }
