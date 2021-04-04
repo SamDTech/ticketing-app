ca954fdd60e78f33281fdaeeb71f60a92ba44404
Damilare <opeyemisamuel222@gmail.com>
2021-04-04 18:59:05 +0100

    error type check

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,8 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-Validation-Error";
 
 export const errorHandler = (err:Error, req:Request, res:Response, next:NextFunction) => {
     console.log('something wrong', err)
 
+    if (err instanceof RequestValidationError) {
+        console.log('handling this as request validation error')
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        console.log('handling database error')
+    }
     res.status(400).send({
         message: err.message
     })
