1ac29acda8f4542b7063e5fb0e8afba8f626af42
Damilare <opeyemisamuel222@gmail.com>
2021-04-04 13:58:48 +0100

    building error handling middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -16,12 +16,13 @@ router.post(
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array())
+       throw new Error('invalid email or password')
     }
 
     const { email, password } = req.body
 
     console.log('creating a user')
+    throw new Error('Error connecting to database')
     res.send({})
   }
 );
