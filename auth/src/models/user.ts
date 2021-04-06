c8c6ca63725a007b77d9c518a4ed19a52fb0d0b8
Damilare <opeyemisamuel222@gmail.com>
2021-04-06 10:41:38 +0100

    user creation

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,6 +7,19 @@ interface UserAttrs {
   password: string;
 }
 
+// interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attr: UserAttrs): UserDoc;
+}
+
+// An interface that describe the properties
+// that a singlr user has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -19,10 +32,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
 
-const buildUser = (attrs: UserAttrs) => {
-    return new User(attrs)
-}
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
-export { User, buildUser };
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { RequestValidationError } from "../errors/request-Validation-Error";
+import { User } from "../models/user";
 
 const router = Router();
 
@@ -21,11 +21,22 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
 
-    const { email, password } = req.body;
+   const { email, password } = req.body;
+
+   const existingUser = await User.findOne({ email })
+
+   if (existingUser) {
+     console.log('Email in use')
+     return res.send({})
+   }
 
 
-    throw new DatabaseConnectionError();
-    res.send({});
+
+   const user = User.build({ email, password })
+
+   await user.save()
+
+res.status(201).send(user)
   }
 );
 
