7c7bbbf52bec0440a68e587678fd4c8349c27b53
Damilare <opeyemisamuel222@gmail.com>
2021-04-07 11:39:29 +0100

    signin work flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,21 @@
 import { Router, Request, Response } from "express";
+import {body, validationResult} from 'express-validator'
+import { RequestValidationError } from "../errors/request-Validation-Error";
 
 const router = Router();
 
-router.post("/api/users/signin", (req: Request, res: Response) => {
+router.post("/api/users/signin", [
+  body('email').withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('Input your password')
+], (req: Request, res: Response) => {
 
-  const {} = req.body
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array())
+  }
+
+  const {email, password} = req.body
   res.send("signin");
 });
 
