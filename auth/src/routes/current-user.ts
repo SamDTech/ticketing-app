883ffb87afdc24116e544917e858033f547ca743
Damilare <opeyemisamuel222@gmail.com>
2021-04-07 18:55:44 +0100

    currentUser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-    res.send('currentuser')
+router.get("/api/users/currentuser", async (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
