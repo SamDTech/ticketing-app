4e6f234ff769e41a7ce3057a22ae26193f924a36
Damilare <opeyemisamuel222@gmail.com>
2021-04-10 13:35:22 +0100

    refactor to use app.js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,55 +1,23 @@
-import express from "express";
-import 'express-async-errors'
-import { json } from "body-parser";
-import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found";
-
-const app = express();
-app.set('trust proxy', true)
-app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}))
-
-
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import mongoose from "mongoose";
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT Key must be defined')
+    throw new Error("JWT Key must be defined");
   }
- try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-    })
-   console.log('connected to mongodb')
- } catch (error) {
-  console.error(error)
- }
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to mongodb");
+  } catch (error) {
+    console.error(error);
+  }
 
   app.listen(3000, () => console.log("Listening on port 3000"));
-}
+};
 
-start()
-
-
+start();
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import jwt from "jsonwebtoken";
-import { BadRequestError } from "../errors/bad-request-error";
 import { currentUser } from "../middlewares/current-user";
 
 const router = Router();
 
 router.get("/api/users/currentuser", currentUser, async (req, res) => {
-  res.send({ currentUser: req.currentUser ||  null });
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
