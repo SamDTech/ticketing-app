efdd36244e0567dcd2451d9e6e6fc28094c8cb41
Damilare <opeyemisamuel222@gmail.com>
2021-04-14 18:45:20 +0100

    conditional rendering of header

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,17 +2,31 @@ import React from "react";
 import Link from "next/link";
 
 const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>
+            <a className="nav-link">{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className="navbar navbar-light bg-light">
-          <Link href="/">
-              <a  className="navbar-brand">GetTip</a>
-          </Link>
+      <Link href="/">
+        <a className="navbar-brand">GetTip</a>
+      </Link>
 
-          <div className="d-flex justify-content-end">
-              <ul className="nav d-flex align-items-center">
-                  {currentUser ? 'SignOut' : 'Sign In/Up'}
-              </ul>
-          </div>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
     </nav>
   );
 };
