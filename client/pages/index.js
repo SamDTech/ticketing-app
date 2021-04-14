cb6f97f7bcf275e7f4be9e1c15bad0b15988da3a
Damilare <opeyemisamuel222@gmail.com>
2021-04-14 15:05:04 +0100

    reuseable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,14 @@
 import axios from "axios";
+import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    // we are on the server
-    // request should be made to http://ingress-nginx.ingress-ingress
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.kube-system.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    // we are in the client
-    // request can be made to a base URL of ''
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get("/api/users/currentuser");
+
+  return data;
 };
 
 export default LandingPage;
