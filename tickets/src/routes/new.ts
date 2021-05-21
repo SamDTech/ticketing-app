7291ea09e4185d3abcbadb8c1b6927ae436fbb85
Damilare <opeyemisamuel222@gmail.com>
2021-05-22 04:53:11 +1000

    test to check the input and adding validations

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,19 @@
-import { requireAuth } from "@samtickets/common";
+import { requireAuth, validateRequest } from "@samtickets/common";
 import { Request, Response, Router } from "express";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/api/tickets",requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().notEmpty().withMessage("title is required"),
+    body("price").isFloat({ gt: 0 }).withMessage('Price must be greater than 0')],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+)
 
 export { router as createTicketRouter };
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,11 +2,12 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import request from "supertest";
 import mongoose from "mongoose";
 import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -39,17 +40,27 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
+global.signin = () => {
   // Build a JWT payload.
+  const payload = {
+    id: "kielsaie",
+    email: "gmail@gmail.com",
+  };
 
   // Create the JWT
-
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
   // Build a session Object. {jwt: myjwt}
 
+  const session = { jwt: token };
+
   // Turn Session into JSON
 
+  const sessionJSON = JSON.stringify(session);
+
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
 
   // return a string that is the cookie with encoded data
+  return [`express:sess=${base64}`];
 };
