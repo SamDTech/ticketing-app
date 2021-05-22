9a4f3663a59b11d9a2c89645850e80aa932271d4
Damilare <opeyemisamuel222@gmail.com>
2021-05-23 00:44:13 +1000

    testing show route

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -48,7 +49,7 @@ it("returns an error if an invalid price is provided", async () => {
       price: -10,
     })
     .expect(400);
-  
+
   await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
@@ -58,4 +59,22 @@ it("returns an error if an invalid price is provided", async () => {
     .expect(400);
 });
 
-it("create a ticket with valid inputs", async () => {});
+it("create a ticket with valid inputs", async () => {
+  // read all records from the ticket collections
+  // let tickets = await Ticket.find({})
+
+  // expect(tickets.length).toEqual(0)
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "jrjrjr",
+      price: 10,
+    })
+    .expect(201);
+
+  // tickets = await Ticket.find({})
+
+  // expect(tickets.length).toEqual(1)
+});
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import { requireAuth, validateRequest } from "@samtickets/common";
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
 
 const router = Router();
 
@@ -9,11 +10,19 @@ router.post(
   requireAuth,
   [
     body("title").not().notEmpty().withMessage("title is required"),
-    body("price").isFloat({ gt: 0 }).withMessage('Price must be greater than 0')],
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
+
+    await ticket.save();
+
+    res.status(201).json(ticket);
   }
-)
+);
 
 export { router as createTicketRouter };
