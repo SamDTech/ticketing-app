b4c1996c634b45ac58bb21b9772ffbf8858986d7
Damilare <opeyemisamuel222@gmail.com>
2021-05-23 07:35:41 +1000

    adding good error logging

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,8 +1,10 @@
 import request from "supertest";
 import { app } from "../../app";
+import  mongoose  from 'mongoose';
 
 it("returns a 404 if the ticket is not found", async () => {
-  await request(app).get("/api/tickets/kfkfkfkfk").send().expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString()
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it("returns the ticket if the ticket is found", async () => {
@@ -10,6 +12,7 @@ it("returns the ticket if the ticket is found", async () => {
   const price = 20;
   const response = await request(app)
     .post("/api/tickets")
+    .set('Cookie', global.signin())
     .send({
       title,
       price,
